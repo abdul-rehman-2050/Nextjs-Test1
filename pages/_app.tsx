0b1fb5ffab55ecc771a5ff7ab93e584ffce8a48f
Abdul Rehman <supermind2002@gmail.com>
2022-09-08 18:56:41 +0500

    mui and navbar added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import { SessionProvider } from "next-auth/react"
 import type { AppProps } from "next/app"
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '../src/theme';
+import { ThemeProvider } from '@mui/material/styles';
+import Head from 'next/head';
 
 // Use the <SessionProvider> to improve performance and allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
@@ -10,7 +14,14 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       // you have a short session maxAge time. Shown here with default values.
       session={pageProps.session}
     >
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   )
 }
