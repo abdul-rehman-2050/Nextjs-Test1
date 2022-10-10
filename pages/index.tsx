37f6fe155e46222369815376676abbdb59e7c7d4
Abdul Rehman <supermind2002@gmail.com>
2022-10-10 10:25:30 +0000

    signin and signup pages added not working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import type { NextPage } from 'next'
 import Layout from "../components/Layout";
+import { useRouter } from 'next/router';
+import { Button } from '@mui/material';
 
 
 const Home: NextPage = () => {
-  
+
+  const router = useRouter();
   return (
     <Layout>
       <h1>NextAuth.js Example</h1>
       <p>
         This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
       </p>
+      <Button variant="contained" color="primary" onClick={() => router.push('/auth/signup')}>
+    Signup
+  </Button>
     </Layout>
   )
 }
